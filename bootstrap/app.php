387ddbd6b5079d467c3d1f5ca666522fc256cfb8
abela-a <abelardhana96@gmail.com>
2024-12-09 21:19:44 +0800

    Add RequestLogMiddleware for logging request details and responses; update logging configuration and .env.example

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Middleware\JsonResponseMiddleware;
+use App\Http\Middleware\RequestLogMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -11,6 +12,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->append([
+            RequestLogMiddleware::class,
+        ]);
+
         $middleware->appendToGroup('api', [
             JsonResponseMiddleware::class,
         ]);
