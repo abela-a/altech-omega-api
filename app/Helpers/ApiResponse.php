b66682847a626f872fef9fcba58036fa7ff83464
abela-a <abelardhana96@gmail.com>
2024-12-09 20:33:20 +0800

    Add ApiResponse helper for standardized API responses and error handling

---
+++ app/Helpers/ApiResponse.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Helpers;
+
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+
+class ApiResponse
+{
+    public static function rollback($error, $message = 'Oops! Process not completed')
+    {
+        DB::rollBack();
+
+        self::throw($error, $message);
+    }
+
+    public static function throw($error, $message = 'Sorry, something went wrong')
+    {
+        Log::error($error);
+
+        throw new HttpResponseException(response()->json(['message' => $message], 500));
+    }
+
+    public static function sendResponse($result, $message, $code = 200)
+    {
+        $response = [
+            'success' => true,
+            'data' => $result,
+        ];
+
+        if (! empty($message)) {
+            $response['message'] = $message;
+        }
+
+        return response()->json($response, $code);
+    }
+}
